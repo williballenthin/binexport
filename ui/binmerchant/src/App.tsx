e842c17a19dc50357b55fa9bb8b0da6f0efda1d1
Willi Ballenthin <willi.ballenthin@gmail.com>
2023-08-31 08:55:16 +0000

    merchant: split rendering from models

--- ui/binmerchant/src/App.tsx
+++ ui/binmerchant/src/App.tsx
@@ -1,211 +1,59 @@
 import './App.css';
 import { Fragment } from 'react';
-import { string, custom } from '@recoiljs/refine';
 import {
-  DefaultValue,
-  atom,
-  selector,
   useRecoilState,
   useRecoilValue,
 } from 'recoil';
 import { BinExport2 } from './BinExport2';
 import Long from 'long';
-import { urlSyncEffect } from 'recoil-sync';
+import { model, getFlowGraphAddress, IFlowGraph, IBasicBlock, IInstruction, IOperand, IExpression } from './Model';
 
-const sha256State = atom({
-  key: "sha256State",
-  default: "0501d09a219131657c54dba71faf2b9d793e466f2c7fdf6b0b3c50ec5b866b2a",
-  effects: [
-    urlSyncEffect({ 
-      history: "push",
-      syncDefault: true,
-      itemKey: "sha256",
-      refine: string() 
-    }),
-  ]
-});
-
-const binExportValue = selector({
-  key: 'binExportValue',
-  get: async ({get}) => {
-    const sha256 = get(sha256State);
-    const raw_be = await (await fetch(`/data/${sha256}.BinExport`)).arrayBuffer();
-
-    return BinExport2.decode(new Uint8Array(raw_be));
-  },
-});
-
-function getFlowGraphAddress(be: BinExport2, fgIndex: number): Long | null {
-  const fg = be.flowGraph[fgIndex];
-
-  const bbIndex = fg.entryBasicBlockIndex;
-  if (bbIndex == null) {
-    return null;
-  }
-  const bb = be.basicBlock[bbIndex];
-  if (bb.instructionIndex == null) {
-    return null;
-  }
-  const ii = bb.instructionIndex[0];
-  if (ii.beginIndex == null) {
-    return null;
-  }
-  const insnIndex = ii.beginIndex;
-
-  const insn = be.instruction[insnIndex];
-
-  const insnAddress = getInstructionAddress(insn);
-  if (insnAddress == null) {
-    // this might be the case if the prior instruction has an address and size.
-    return null;
-  }
-
-  return insnAddress
+function addressId(address: Long): string {
+  return `address-${address.toString(0x10).toUpperCase()}`;
 }
 
-const defaultAddressValue = selector({
-  key: 'defaultAddressValue',
-  get: ({get}) => {
-    const be = get(binExportValue);
-    const address = getFlowGraphAddress(be, 0);
-    if (address == null) {
-      throw new Error("first function has no address");
-    }
-    return address;
-  },
-});
+function addressKey(address: Long): string {
+  return address.toString(0x10);
+}
 
-const currentAddressState = atom({
-  key: "addressState",
-  default: defaultAddressValue,
-  effects: [
-    urlSyncEffect({ 
-      history: "push",
-      syncDefault: true,
-      itemKey: "address",
-      refine: custom(x => Long.isLong(x) ? x : null),
-      read: ({read}) => {
-        const v = read("address");
-
-        if (v instanceof DefaultValue) {
-          return Long.fromNumber(0);
-        }
-
-        return Long.fromString(read("address") as any, true, 0x10);
-      },
-      write: ({write, read}, newValue) => {
-        write("address", newValue.toString(0x10))
-      },
-    })
-  ]
-});
-
-const binExportIndexValue = selector({
-  key: "binExportIndexValue",
-  get: ({get}) => {
-    const be: BinExport2 = get(binExportValue)
-
-    // from address (as hex) to insn index
-    const insnByAddress: Record<string, number> = {};
-    for (const [index, insn] of be.instruction.entries()) {
-      const iaddr = getInstructionAddress(insn);
-      if (iaddr == null) {
-        continue;
-      }
-
-      insnByAddress[iaddr.toString(16)] = index;
-    }
-
-    // from insn index to bb index
-    const bbByInsn: Record<number, number> = {};
-    for (const [index, bb] of be.basicBlock.entries()) {
-      if (bb.instructionIndex == null) {
-        continue;
-      }
-
-      const ii = bb.instructionIndex[0];
-      if (ii.beginIndex == null) {
-        continue
-      }
-
-      bbByInsn[ii.beginIndex] = index;
-    }
-
-    const fgByBb: Record<number, number> = {};
-    for (const [index, fg] of be.flowGraph.entries()) {
-      if (fg.entryBasicBlockIndex == null) {
-        continue
-      }
-
-      fgByBb[fg.entryBasicBlockIndex] = index;
-    }
-
-    function getFlowGraphByAddress(address: Long): number | null {
-      const insnIndex = insnByAddress[address.toString(16)];
-      if (insnIndex == null) {
-        return null;
-      }
-
-      const bbIndex = bbByInsn[insnIndex];
-      if (bbIndex == null) {
-        return null;
-      }
-
-      const fgIndex = fgByBb[bbIndex];
-      if (fgIndex == null) {
-        return null;
-      }
-
-      return fgIndex;
-    }
-
-    return {
-      getFlowGraphByAddress
-    }
-  }
-})
-
-const currentFlowGraphIndexValue = selector({
-  key: 'currentFlowGraphIndexValue',
-  get: ({get}) => {
-    const currentAddress = get(currentAddressState);
-    const beIndex = get(binExportIndexValue);
-
-    return beIndex.getFlowGraphByAddress(currentAddress)
-  },
-});
+function Address({address}: {address: Long}) {
+  return (<span className="address">{address.toString(0x10).toUpperCase()}</span>);
+}
 
 function Meta({meta}: {meta: BinExport2.IMeta | null | undefined}) {
-  const [currentAddress, setCurrentAddress] = useRecoilState(currentAddressState);
+  const currentAddress = useRecoilValue(model.currentAddress);
 
-  if (meta) {
-    return (
-      <div>
-        <dl>
-          <dt>arch</dt>
-          <dd>{meta.architectureName}</dd>
-
-          <dt>executable id</dt>
-          <dd>{meta.executableId}</dd>
-
-          <dt>executeable name</dt>
-          <dd>{meta.executableName}</dd>
-
-          <dt>address</dt>
-          <dd>{currentAddress.toString(0x10).toUpperCase()}</dd>
-        </dl>
-      </div>
-    )
-  } else {
-    return (
-      <dl></dl>
-    )
+  if (meta == null) {
+    return (<div>
+      <dl>
+        <dt>meta</dt>
+        <dd>(none)</dd>
+      </dl>
+    </div>)
   }
+
+  return (
+    <div>
+      <dl>
+        <dt>arch</dt>
+        <dd>{meta.architectureName}</dd>
+
+        <dt>executable id</dt>
+        <dd>{meta.executableId}</dd>
+
+        <dt>executeable name</dt>
+        <dd>{meta.executableName}</dd>
+
+        <dt>address</dt>
+        <dd><Address address={currentAddress} /></dd>
+      </dl>
+    </div>
+  )
 }
 
 function FunctionList() {
-  const be = useRecoilValue(binExportValue);
-  const [currentAddress, setCurrentAddress] = useRecoilState(currentAddressState);
+  const be = useRecoilValue(model.be);
+  const [currentAddress, setCurrentAddress] = useRecoilState(model.currentAddress);
 
   const addresses = [];
   for (const fgIndex of be.flowGraph.keys()) {
@@ -220,9 +68,9 @@ function FunctionList() {
   return (
     <ul>
       {addresses.map((address) => (
-        <li key={address.toString(0x10)} style={{fontWeight: address.eq(currentAddress) ? "bold" : "normal"}}>
+        <li key={addressKey(address)} style={{fontWeight: address.eq(currentAddress) ? "bold" : "normal"}}>
           <a onClick={() => setCurrentAddress(address)}>
-            {address.toString(0x10).toUpperCase()}
+            <Address address={address} />
           </a>
         </li>
       ))}
@@ -230,15 +78,9 @@ function FunctionList() {
   );
 }
 
-function Expression({be, index, expr, childrenByIndex}: {be: BinExport2, index: number, expr: BinExport2.IExpression, childrenByIndex: Record<number, Array<number>>}) {
-  if (expr.type == null) {
-    return (<div>error: no expression type</div>)
-  };
-
-  const childrenIndices = childrenByIndex[index];
-
-  const children = childrenIndices.map((childIndex, index) => (
-    <Expression be={be} index={childIndex} expr={be.expression[childIndex]} childrenByIndex={childrenByIndex} key={index} />
+function Expression({expr}: {expr: IExpression}) {
+  const children = expr.children.map((child, index) => (
+    <Expression expr={child} key={index} />
   ));
 
   switch (expr.type) {
@@ -279,74 +121,41 @@ function Expression({be, index, expr, childrenByIndex}: {be: BinExport2, index: 
   }
 }
 
-function Operand({be, op}: {be: BinExport2, op: BinExport2.IOperand}) {
-  if (op.expressionIndex == null) {
-    return (<div>error: operand has no expression</div>);
-  }
-
-  const childrenByIndex: Record<number, Array<number>> = {0: []};
-  op.expressionIndex.forEach((exprIndex) => {
-    childrenByIndex[exprIndex] = []; 
-    const expr = be.expression[exprIndex];
-    if (expr.parentIndex != null && expr.parentIndex !== exprIndex) {
-      childrenByIndex[expr.parentIndex].push(exprIndex);
-    }
-  });
-
-  const childIndex0 = op.expressionIndex[0];
-
+function Operand({op}: {op: IOperand}) {
   return (
     <span className="operand">
-      <Expression be={be} index={childIndex0} expr={be.expression[childIndex0]} childrenByIndex={childrenByIndex} />
+      <Expression expr={op.expression} />
     </span>
   )
 }
 
-function Instruction({be, address, insn}: {be: BinExport2, address: Long, insn: BinExport2.IInstruction}) {
-  const beIndex = useRecoilValue(binExportIndexValue);
-  const [currentAddress, setCurrentAddress] = useRecoilState(currentAddressState);
-
-  if (insn.mnemonicIndex == null) {
-    return (<div>error: instruction has no mnemonic</div>);
-  }
-
-  const mnem = be.mnemonic[insn.mnemonicIndex];
-
-  const targets: Array<Long> = [];
-  if (insn.callTarget != null) {
-    for (const callTarget of insn.callTarget) {
-      const callTargetAddress = beIndex.getFlowGraphByAddress(ensureLong(callTarget))
-      if (callTargetAddress == null) {
-        continue;
-      }
-      targets.push(ensureLong(callTarget));
-    }
-  }
+function Instruction({insn}: {insn: IInstruction}) {
+  const [_, setCurrentAddress] = useRecoilState(model.currentAddress);
 
   return (
     <span className="instruction">
-      <span className="address" id={address.toString(0x10)}>
-        {address.toString(16).toUpperCase()}
+      <span className="address" id={addressId(insn.address)}>
+        <Address address={insn.address} />
       </span>
 
       <span className="bytes"></span>
 
-      <span className="mnemonic">{mnem.name}</span>
+      <span className="mnemonic">{insn.mnemonic}</span>
 
       <span className="operands">
-        {insn.operandIndex?.map((opIndex, index) => (
+        {insn.operands.map((operand, index) => (
           <Fragment key={index}>
             {!! index && ", "}
-            <Operand be={be} op={be.operand[opIndex]} key={index} />
+            <Operand op={operand} key={index} />
           </Fragment>
         ))}
       </span>
 
       <span className="comment">
-        {targets.map((target) => (
-          <Fragment key={target.toString(0x10)}>
+        {insn.callTargets.map((target) => (
+          <Fragment key={addressKey(target)}>
             <a onClick={() => setCurrentAddress(target)}>
-              →{target.toString(0x10).toUpperCase()}
+              →<Address address={target} />
             </a>
             {" "}
           </Fragment>
@@ -356,100 +165,31 @@ function Instruction({be, address, insn}: {be: BinExport2, address: Long, insn: 
   )
 }
 
-function* getBasicBlockInstructionIndices(bb: BinExport2.IBasicBlock) {
-  if (!bb.instructionIndex) {
-    return;
-  }
-
-  for (const insnRange of bb.instructionIndex) {
-    if (insnRange.endIndex != null && insnRange.beginIndex != null) {
-      for (let insnIndex = insnRange.beginIndex; insnIndex < insnRange.endIndex; insnIndex++) {
-        yield insnIndex;
-      }
-    } else if (insnRange.beginIndex != null) {
-      yield insnRange.beginIndex;
-    } else {
-    }
-  }
-
-  return;
-}
-
-function ensureLong(v: number | Long): Long  {
-  if (Long.isLong(v)) {
-    return v;
-  } else if (Number.isInteger(v)) {
-    return Long.fromString(v.toString(0x10), true, 0x10);
-  } else {
-    throw new Error("unexpected type");
-  }
-}
-
-function getInstructionAddress(insn: BinExport2.IInstruction): Long | null {
-  if (insn.address == null) {
-    return null;
-  } else if (Long.isLong(insn.address)) {
-    return insn.address;
-  } else if (Number.isInteger(insn.address) && insn.address !== 0x0) {
-    // I'm quite sure how unsafe integers are being passed back here.
-    // but translating them to strings and then back to longs seems to work.
-    return Long.fromString(insn.address.toString(0x10), true, 0x10);
-  } else if (Number.isInteger(insn.address) && insn.address === 0x0) {
-    return null;
-  } else {
-    return null;
-  }
-}
-
-function BasicBlock({be, bb}: {be: BinExport2, bb: BinExport2.IBasicBlock}) {
-  if (bb.instructionIndex == null) {
-    return (<div>error: basic block has no instruction index</div>);
-  }
-
-  let address: Long = new Long(0);
-  const instructions = [];
-  for (const insnIndex of getBasicBlockInstructionIndices(bb)) {
-    const insn = be.instruction[insnIndex];
-
-    const iaddr = getInstructionAddress(insn);
-    if (iaddr != null) {
-      address = iaddr;
-    }
-
-    instructions.push(<Instruction be={be} insn={be.instruction[insnIndex]} address={address} key={address.toString(16)} />)
-
-    if (insn.rawBytes != null) {
-      address = address.add(new Long(insn.rawBytes.length));
-    }
-  }
-
+function BasicBlock({bb}: {bb: IBasicBlock}) {
   return (
     <div className="basic-block">
-      {instructions}
-    </div>
-  )
-}
-
-function FlowGraph({be, fg}: {be: BinExport2, fg: BinExport2.IFlowGraph}) {
-  if (fg.basicBlockIndex == null) {
-    return (<div>error: flow graph has no basic block index</div>);
-  }
-
-  return (
-    <div className="flow-graph">
-      {fg.basicBlockIndex.map((bbIndex, index) => (
-        <BasicBlock be={be} bb={be.basicBlock[bbIndex]} key={index} />
+      {bb.instructions.map((insn) => (
+        <Instruction insn={insn} key={insn.index} />
       ))}
     </div>
   )
 }
 
+function FlowGraph({fg}: {fg: IFlowGraph}) {
+  return (
+    <div className="flow-graph">
+      {fg.basicBlocks.map((bb) => (
+        <BasicBlock bb={bb} key={bb.index} />
+      ))}
+    </div>
+  )
+}
 
 function App() {
-  const [sha256, setSha256] = useRecoilState(sha256State);
-  const be: BinExport2 = useRecoilValue(binExportValue)
-  const [currentAddress, setCurrentAddress] = useRecoilState(currentAddressState);
-  const currentFlowGraphIndex = useRecoilValue(currentFlowGraphIndexValue);
+  const sha256 = useRecoilValue(model.sha256);
+  const be: BinExport2 = useRecoilValue(model.be)
+  const currentAddress = useRecoilValue(model.currentAddress);
+  const currentFlowGraph = useRecoilValue(model.currentFlowGraph);
 
   document.title = `BinMerchant: ${sha256}: ${currentAddress}`;
 
@@ -465,7 +205,7 @@ function App() {
     <div className="App">
       <Meta meta={be.metaInformation} />
       <div style={{position: "relative"}}>
-        {currentFlowGraphIndex == null ? "" : <FlowGraph be={be} fg={be.flowGraph[currentFlowGraphIndex]} />}
+        {currentFlowGraph == null ? "" : <FlowGraph fg={currentFlowGraph} />}
         <div style={{position: "absolute", right: "0px", top:  "0px", height: "calc(100vh - 200px)", overflow: "scroll"}}>
           <FunctionList />
         </div>
---
+++ ui/binmerchant/src/Model.ts
@@ -0,0 +1,441 @@
+import { string, custom } from '@recoiljs/refine';
+import {
+  DefaultValue,
+  RecoilValueReadOnly,
+  atom,
+  selector,
+} from 'recoil';
+import { BinExport2 } from './BinExport2';
+import Long from 'long';
+import { urlSyncEffect } from 'recoil-sync';
+
+function ensureLong(v: number | Long): Long  {
+  if (Long.isLong(v)) {
+    return v;
+  } else if (Number.isInteger(v)) {
+    return Long.fromString(v.toString(0x10), true, 0x10);
+  } else {
+    throw new Error("unexpected type");
+  }
+}
+
+export function getInstructionAddress(insn: BinExport2.IInstruction): Long | null {
+  if (insn.address == null) {
+    return null;
+  } else if (Long.isLong(insn.address)) {
+    return insn.address;
+  } else if (Number.isInteger(insn.address) && insn.address !== 0x0) {
+    // I'm quite sure how unsafe integers are being passed back here.
+    // but translating them to strings and then back to longs seems to work.
+    return Long.fromString(insn.address.toString(0x10), true, 0x10);
+  } else if (Number.isInteger(insn.address) && insn.address === 0x0) {
+    return null;
+  } else {
+    return null;
+  }
+}
+
+function* getBasicBlockInstructionIndices(bb: BinExport2.IBasicBlock) {
+  if (!bb.instructionIndex) {
+    return;
+  }
+
+  for (const insnRange of bb.instructionIndex) {
+    if (insnRange.endIndex != null && insnRange.beginIndex != null) {
+      for (let insnIndex = insnRange.beginIndex; insnIndex < insnRange.endIndex; insnIndex++) {
+        yield insnIndex;
+      }
+    } else if (insnRange.beginIndex != null) {
+      yield insnRange.beginIndex;
+    } else {
+    }
+  }
+
+  return;
+}
+
+export function getFlowGraphAddress(be: BinExport2, fgIndex: number): Long | null {
+  const fg = be.flowGraph[fgIndex];
+
+  const bbIndex = fg.entryBasicBlockIndex;
+  if (bbIndex == null) {
+    return null;
+  }
+  const bb = be.basicBlock[bbIndex];
+  if (bb.instructionIndex == null) {
+    return null;
+  }
+  const ii = bb.instructionIndex[0];
+  if (ii.beginIndex == null) {
+    return null;
+  }
+  const insnIndex = ii.beginIndex;
+
+  const insn = be.instruction[insnIndex];
+
+  const insnAddress = getInstructionAddress(insn);
+  if (insnAddress == null) {
+    // this might be the case if the prior instruction has an address and size.
+    return null;
+  }
+
+  return insnAddress
+}
+
+const sha256State = atom({
+  key: "sha256State",
+  default: "0501d09a219131657c54dba71faf2b9d793e466f2c7fdf6b0b3c50ec5b866b2a",
+  effects: [
+    urlSyncEffect({ 
+      history: "push",
+      syncDefault: true,
+      itemKey: "sha256",
+      refine: string() 
+    }),
+  ]
+});
+
+const binExportValue = selector({
+  key: 'binExportValue',
+  get: async ({get}) => {
+    const sha256 = get(sha256State);
+    const raw_be = await (await fetch(`/data/${sha256}.BinExport`)).arrayBuffer();
+
+    const be = BinExport2.decode(new Uint8Array(raw_be));
+    console.log(be);
+
+    return be;
+  },
+});
+
+const defaultAddressValue = selector({
+  key: 'defaultAddressValue',
+  get: ({get}) => {
+    const be = get(binExportValue);
+    const address = getFlowGraphAddress(be, 0);
+    if (address == null) {
+      throw new Error("first function has no address");
+    }
+    return address;
+  },
+});
+
+const currentAddressState = atom({
+  key: "addressState",
+  default: defaultAddressValue,
+  effects: [
+    urlSyncEffect({ 
+      history: "push",
+      syncDefault: true,
+      itemKey: "address",
+      refine: custom(x => Long.isLong(x) ? x : null),
+      read: ({read}) => {
+        const v = read("address");
+
+        if (v instanceof DefaultValue) {
+          return Long.fromNumber(0);
+        }
+
+        return Long.fromString(read("address") as any, true, 0x10);
+      },
+      write: ({write, read}, newValue) => {
+        write("address", newValue.toString(0x10))
+      },
+    })
+  ]
+});
+
+const binExportIndexValue = selector({
+  key: "binExportIndexValue",
+  get: ({get}) => {
+    const be: BinExport2 = get(binExportValue)
+
+    // from address (as hex) to insn index
+    const insnByAddress: Record<string, number> = {};
+    for (const [index, insn] of be.instruction.entries()) {
+      const iaddr = getInstructionAddress(insn);
+      if (iaddr == null) {
+        continue;
+      }
+
+      insnByAddress[iaddr.toString(16)] = index;
+    }
+
+    // from insn index to bb index
+    const bbByInsn: Record<number, number> = {};
+    for (const [index, bb] of be.basicBlock.entries()) {
+      if (bb.instructionIndex == null) {
+        continue;
+      }
+
+      const ii = bb.instructionIndex[0];
+      if (ii.beginIndex == null) {
+        continue
+      }
+
+      bbByInsn[ii.beginIndex] = index;
+    }
+
+    const fgByBb: Record<number, number> = {};
+    for (const [index, fg] of be.flowGraph.entries()) {
+      if (fg.entryBasicBlockIndex == null) {
+        continue
+      }
+
+      fgByBb[fg.entryBasicBlockIndex] = index;
+    }
+
+    function getFlowGraphByAddress(address: Long): number | null {
+      const insnIndex = insnByAddress[address.toString(16)];
+      if (insnIndex == null) {
+        return null;
+      }
+
+      const bbIndex = bbByInsn[insnIndex];
+      if (bbIndex == null) {
+        return null;
+      }
+
+      const fgIndex = fgByBb[bbIndex];
+      if (fgIndex == null) {
+        return null;
+      }
+
+      return fgIndex;
+    }
+
+    return {
+      getFlowGraphByAddress
+    }
+  }
+})
+
+const currentFlowGraphIndexValue = selector({
+  key: 'currentFlowGraphIndexValue',
+  get: ({get}) => {
+    const currentAddress = get(currentAddressState);
+    const beIndex = get(binExportIndexValue);
+
+    return beIndex.getFlowGraphByAddress(currentAddress);
+  },
+});
+
+export interface IExpression {
+  index: number;
+  type: BinExport2.Expression.Type;
+  symbol?: string;
+  immediate?: Long;
+  isRelocation: boolean;
+  children: IExpression[];
+}
+
+export interface IOperand {
+  index: number;
+  expression: IExpression;
+}
+
+export interface IInstruction {
+  index: number;
+  address: Long;
+  callTargets: Long[];
+  mnemonic: string;
+  operands: IOperand[];
+  rawBytes: Uint8Array;
+  comments: BinExport2.IComment[]; // TODO
+}
+
+export interface IBasicBlock {
+  index: number;
+  instructions: IInstruction[];
+}
+
+export interface IFlowGraph {
+  index: number;
+  basicBlocks: IBasicBlock[];
+  entryBasicBlockIndex: number;
+}
+
+const currentFlowGraphValue: RecoilValueReadOnly<IFlowGraph | null> = selector({
+  key: 'currentFlowGraph',
+  get: ({get}) => {
+    const be = get(binExportValue);
+    const currentAddress = get(currentAddressState);
+    const beIndex = get(binExportIndexValue);
+
+    try {
+      const fgIndex = beIndex.getFlowGraphByAddress(currentAddress);
+      if (fgIndex == null) {
+        throw new Error("missing flow graph");
+      }
+      const fg = be.flowGraph[fgIndex];
+
+      if (fg.basicBlockIndex == null) {
+        throw new Error("missing basic block");
+      }
+
+      if (fg.entryBasicBlockIndex == null) {
+        throw new Error("missing basic block entry");
+      }
+
+      if (fg.edge == null) {
+        throw new Error("missing edge");
+      }
+
+      const addressesByInsn: Record<number, Long> = {};
+      let address: Long = new Long(0);
+
+      for (const bbIndex of fg.basicBlockIndex) {
+        const bb = be.basicBlock[bbIndex];
+
+        for (const insnIndex of getBasicBlockInstructionIndices(bb)) {
+          const insn = be.instruction[insnIndex];
+
+          const iaddr = getInstructionAddress(insn);
+          if (iaddr != null) {
+            // instruction has an explicit address,
+            // such as if it was not-contiguous with prior bb,
+            // so use that.
+            address = iaddr;
+          }
+
+          addressesByInsn[insnIndex] = address;
+
+          if (insn.rawBytes != null) {
+            address = address.add(new Long(insn.rawBytes.length));
+          }
+        }
+      }
+
+      return {
+        index: fgIndex,
+        entryBasicBlockIndex: fg.entryBasicBlockIndex,
+        basicBlocks: fg.basicBlockIndex.map((bbIndex) => {
+          const bb = be.basicBlock[bbIndex];
+
+          if (bb.instructionIndex == null) {
+            throw new Error("missing instructions");
+          }
+
+          return {
+            index: bbIndex,
+            instructions: [...getBasicBlockInstructionIndices(bb)].map((insnIndex) => {
+              const insn = be.instruction[insnIndex];
+
+              if (insn.callTarget == null) {
+                throw new Error("missing call target");
+              }
+
+              if (insn.mnemonicIndex == null) {
+                throw new Error("missing mnemonic");
+              }
+
+              const mnem = be.mnemonic[insn.mnemonicIndex];
+              if (mnem.name == null) {
+                throw new Error("missing name");
+              }
+
+              if (insn.operandIndex == null) {
+                throw new Error("missing operands");
+              }
+
+              if (insn.rawBytes == null) {
+                throw new Error("missing bytes");
+              }
+
+              if (insn.commentIndex == null) {
+                throw new Error("missing comments");
+              }
+
+              return {
+                index: insnIndex,
+                address: addressesByInsn[insnIndex],
+                callTargets: insn.callTarget.map(ensureLong),
+                mnemonic: mnem.name,
+                operands: insn.operandIndex.map((opIndex) => {
+                  const op = be.operand[opIndex];
+                  if (op == null) {
+                    throw new Error("operand not found");
+                  }
+
+                  if (op.expressionIndex == null) {
+                    throw new Error("missing expressions");
+                  }
+
+                  const childrenByIndex: Record<number, Array<number>> = {0: []};
+                  op.expressionIndex.forEach((exprIndex) => {
+                    childrenByIndex[exprIndex] = []; 
+                    const expr = be.expression[exprIndex];
+                    if (expr.parentIndex != null && expr.parentIndex !== exprIndex) {
+                      childrenByIndex[expr.parentIndex].push(exprIndex);
+                    }
+                  });
+
+                  function buildExpressions(exprIndex: number): IExpression {
+                    const expr = be.expression[exprIndex];
+
+                    if (expr.type == null) {
+                      throw new Error("missing type");
+                    }
+
+                    if (expr.isRelocation == null) {
+                      throw new Error("missing relocation");
+                    }
+
+                    const ret: IExpression = {
+                      index: exprIndex,
+                      type: expr.type,
+                      isRelocation: expr.isRelocation,
+                      children: childrenByIndex[exprIndex].map(buildExpressions),
+                    };
+
+                    if (Object.hasOwn(expr, "symbol")) {
+                      if (expr.symbol == null) {
+                        throw new Error("missing symbol");
+                      }
+                      ret.symbol = expr.symbol;
+                    }
+
+                    if (Object.hasOwn(expr, "immediate")) {
+                      if (expr.immediate == null) {
+                        throw new Error("missing immediate");
+                      }
+                      ret.immediate = ensureLong(expr.immediate);
+                    }
+
+                    return ret;
+                  }
+
+                  return {
+                    index: opIndex,
+                    expression: buildExpressions(op.expressionIndex[0]),
+                  }
+                }),
+                rawBytes: insn.rawBytes,
+                comments: insn.commentIndex.map((commentIndex) => {
+                  const comment = be.comment[commentIndex];
+                  if (comment == null) {
+                    throw new Error("comment not found");
+                  }
+
+                  return comment;
+                }),
+              }
+            })
+          }
+        }),
+      }
+    } catch (e) {
+      console.error("error: ", e);
+      return null;
+    }
+  },
+})
+
+export const model = {
+    sha256: sha256State,
+    be: binExportValue,
+    index: binExportIndexValue,
+    currentAddress: currentAddressState,
+    currentFlowGraphIndex: currentFlowGraphIndexValue,
+    currentFlowGraph: currentFlowGraphValue,
+}
